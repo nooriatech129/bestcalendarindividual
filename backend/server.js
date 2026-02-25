73782161df84efa9cef57e079719437282c62966
nooriatech129 <nsediqi8770@sdsu.edu>
2026-02-25 11:30:22 -0800

    Set up Express server with CORS and events endpoint
    Fix typos in require statements and adjust server setup.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
+const express = requier("express");
+const cors = requier("cors");
+const calendarApp = express();
+calendarApp.use(cors());
+calendarApp.use( express.json());
+calendarApp.get("/events", (req, res) => {
+    res.json ([
+        { title: "subjects name" , date: "dates"}
+        ]);
+    const port = 3000;
+    calendarApp.listen(port), () => {
+    console.log("Backend running on port 3000");
+    });
+        
 
