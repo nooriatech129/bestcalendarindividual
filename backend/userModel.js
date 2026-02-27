30ff52a0a55cdd72fec568a389897051fa10fca6
nooriatech129 <nsediqi8770@sdsu.edu>
2026-02-26 21:37:34 -0800

    Implement user management functions in userModel.js

--- backend/userModel.js
+++ backend/userModel.js
@@ -1,1 +1,18 @@
+let users = [];
+function addUser(email, password) {
+    const user = {
+        id: users.length + 1,
+        email: email,
+        password: password
+    };
 
+    users.push(user);
+    return user;
+}
+function findUserByEmail(email) {
+    return users.find(user => user.email === email);
+}
+module.exports = {
+    addUser,
+    findUserByEmail
+};
